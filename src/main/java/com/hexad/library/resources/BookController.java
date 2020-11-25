0e82569c0e00ce103dd3925b4080d6337d0a4c1e
gtiwari <gouravtiwari489@gmail.com>
2020-11-25 18:56:16 +0530

    [Gourav]: CRUD operations for BookController and unit tests

--- src/main/java/com/hexad/library/resources/BookController.java
+++ src/main/java/com/hexad/library/resources/BookController.java
@@ -4,10 +4,7 @@ import com.hexad.library.model.Book;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.logging.Logger;
 
@@ -17,9 +14,24 @@ import java.util.logging.Logger;
 public class BookController {
 
     @PostMapping
-    public ResponseEntity<Book> addBook(@RequestBody Book book){
+    public ResponseEntity<Book> save(@RequestBody Book book){
         log.info("Input request to add books {}", book);
         return ResponseEntity.status(HttpStatus.CREATED).body(null);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> getById(@PathVariable Integer id){
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
+
+    @GetMapping
+    public ResponseEntity<Book> getAll(){
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Book> delete(@PathVariable Integer id){
+        return ResponseEntity.status(HttpStatus.OK).body(null);
     }
 
 }
--- src/test/java/com/hexad/library/resources/BookControllerTest.java
+++ src/test/java/com/hexad/library/resources/BookControllerTest.java
@@ -8,10 +8,11 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -30,9 +31,39 @@ public class BookControllerTest {
 
     @Test
     public void shouldAddBooksForValidInput() throws Exception {
-        mockMvc.perform(post("/books").contentType(MediaType.APPLICATION_JSON)
-                .content("{\"bookName\":\"book1\"}").accept(MediaType.APPLICATION_JSON)).andExpect(status().isCreated());
+        mockMvc.perform(post("/books")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"bookName\":\"book1\"}")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status()
+                .isCreated());
     }
 
+    @Test
+    public void shouldGetBookById() throws Exception
+    {
+        mockMvc.perform(get("/books/{id}", 1)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+                //.andExpect(MockMvcResultMatchers.jsonPath("$.id")
+               // .value(1));
+    }
+
+    @Test
+    public void shouldGetAllBooks() throws Exception
+    {
+        mockMvc.perform(get("/books")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+        //.andExpect(MockMvcResultMatchers.jsonPath("$.id")
+        // .value(1));
+    }
+
+    @Test
+    public void shouldDeleteBook() throws Exception {
+        mockMvc.perform(delete("/books/{id}", 1)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
 
 }
